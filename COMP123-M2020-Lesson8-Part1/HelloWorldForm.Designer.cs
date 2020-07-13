b4ded231b9c15c738c2fb1992959cb06e4196d83
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2020-07-13 12:25:36 -0400

    added Name TextBox

--- COMP123-M2020-Lesson8-Part1/HelloWorldForm.Designer.cs
+++ COMP123-M2020-Lesson8-Part1/HelloWorldForm.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.WelcomeLabel = new System.Windows.Forms.Label();
             this.ClickMeButton = new System.Windows.Forms.Button();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.NameLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // WelcomeLabel
@@ -37,9 +39,9 @@
             this.WelcomeLabel.BackColor = System.Drawing.SystemColors.Control;
             this.WelcomeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.WelcomeLabel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.WelcomeLabel.Location = new System.Drawing.Point(231, 190);
+            this.WelcomeLabel.Location = new System.Drawing.Point(12, 190);
             this.WelcomeLabel.Name = "WelcomeLabel";
-            this.WelcomeLabel.Size = new System.Drawing.Size(178, 47);
+            this.WelcomeLabel.Size = new System.Drawing.Size(607, 47);
             this.WelcomeLabel.TabIndex = 0;
             this.WelcomeLabel.Text = "Hello, World!";
             this.WelcomeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -53,17 +55,40 @@
             this.ClickMeButton.Location = new System.Drawing.Point(252, 251);
             this.ClickMeButton.Name = "ClickMeButton";
             this.ClickMeButton.Size = new System.Drawing.Size(136, 47);
-            this.ClickMeButton.TabIndex = 1;
+            this.ClickMeButton.TabIndex = 2;
             this.ClickMeButton.Text = "Click Me!";
             this.ClickMeButton.UseVisualStyleBackColor = false;
             this.ClickMeButton.Click += new System.EventHandler(this.ClickMeButton_Click);
             // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameTextBox.Location = new System.Drawing.Point(212, 111);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(217, 38);
+            this.NameTextBox.TabIndex = 1;
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.NameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameLabel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.NameLabel.Location = new System.Drawing.Point(187, 50);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(267, 47);
+            this.NameLabel.TabIndex = 0;
+            this.NameLabel.Text = "Enter Your Name";
+            this.NameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // HelloWorldForm
             // 
+            this.AcceptButton = this.ClickMeButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.NameTextBox);
             this.Controls.Add(this.ClickMeButton);
+            this.Controls.Add(this.NameLabel);
             this.Controls.Add(this.WelcomeLabel);
             this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -80,6 +105,8 @@
 
         private System.Windows.Forms.Label WelcomeLabel;
         private System.Windows.Forms.Button ClickMeButton;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.Label NameLabel;
     }
 }
 
